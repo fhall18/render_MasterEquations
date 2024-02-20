f38ff05ac7e7ad1ece1134ad7aa3b956fb099d50
fhall18 <frederickchall@gmail.com>
2024-02-20 17:23:36 -0500

    updated charts

--- src/app.py
+++ src/app.py
@@ -1,6 +1,7 @@
 
 from dash import Dash, html, dcc, callback, Output, Input
 import plotly.graph_objs as go
+from plotly.subplots import make_subplots
 import seaborn as sns
 import numpy as np
 from model import ThermalState
@@ -19,12 +20,9 @@ server = app.server
 app.layout = html.Div([
     html.H1("Thermal State Master Equations"),
     html.Div([
-        dcc.Graph(id='state-chart'),
-    ], style={'display': 'inline-block', 'width': '49%'}),
-
-    html.Div([
-        dcc.Graph(id='dist-chart'),
-    ], style={'display': 'inline-block', 'width': '49%'}),
+        dcc.Graph(id='main-chart'),
+    ],
+    ),
 
     html.Div([
         html.H1("Thermal Inputs"),
@@ -32,9 +30,9 @@ app.layout = html.Div([
             html.H3("Outdoor Air Temperature (Ta)"),
             dcc.Slider(
                 id='Ta-slider',
-                min=0,
-                max=30,
-                value=10,
+                min=-15,
+                max=15,
+                value=0,
                 step=1
             )
         ]),
@@ -65,9 +63,9 @@ app.layout = html.Div([
 
 
 @app.callback(
-
-    Output('state-chart', 'figure'),
-    Output('dist-chart', 'figure'),
+Output('main-chart', 'figure'),
+    # Output('state-chart', 'figure'),
+    # Output('dist-chart', 'figure'),
     [Input('Ta-slider', 'value'),
      Input('Tset-slider', 'value'),
      Input('Tstart-slider', 'value')]
@@ -87,32 +85,24 @@ def update_chart(Ta,Tset,Tstart):
     d_line_colors_rgba = ['rgba({}, {}, {}, 1)'.format(int(r * 255), int(g * 255), int(b * 255)) for r, g, b in
                           d_list]
 
-
-    state_fig = go.Figure()
-    dist_fig = go.Figure()
+    fig = make_subplots(rows=1, cols=2, shared_yaxes=False,shared_xaxes=False, vertical_spacing=0.1)
 
     for idx, t in enumerate(np.arange(0, 25, 4)):
         if t > 0:
             state = np.sum(x_path[t], axis=0)[0].tolist() + np.sum(x_path[t], axis=0)[1].tolist()
             dist = np.sum(np.sum(x_path[t], axis=2), axis=1)
 
-            state_fig.add_trace(go.Scatter(x=x1, y=state, mode='lines+markers', line=dict(dash='dash',color=s_line_colors_rgba[idx-1]), name=f't = {t}'))
-            dist_fig.add_trace(go.Scatter(x=list(range(0, 50)), y=dist, mode='lines', line=dict(dash='dash',color=d_line_colors_rgba[idx-1]), name=f't = {t}'))
+            fig.add_trace(go.Scatter(x=x1, y=state, mode='lines+markers', line=dict(dash='dash',color=s_line_colors_rgba[idx-1]), name=f't = {t}',showlegend=False), row=1, col=1)
+            fig.add_trace(go.Scatter(x=list(range(0, 50)), y=dist, mode='lines', line=dict(dash='dash',color=d_line_colors_rgba[idx-1]), name=f't = {t}'), row=1, col=2)
 
     # Update layout
-    state_fig.update_layout(title='Thermal State Distribution',
-                      xaxis=dict(title="States", tickvals=x1, ticktext=['S[0,0]', 'S[0,1]','S[1,0]','S[1,1]']),
-                      yaxis_title='Occupation number')
+    fig.update_yaxes(title_text="Occupation Number", row=1, col=1)
+    fig.update_xaxes(title_text="Thermal State",tickvals=x1, ticktext=['S[0,0]', 'S[0,1]','S[1,0]','S[1,1]'], row=1, col=1)
 
-    dist_fig.update_layout(title='Indoor Air Temperature (Ti) Distribution',
-                      xaxis_title='Indoor Temperature Ti (C)',
-                      yaxis_title='Density')
+    fig.update_xaxes(title_text="Indoor Air Temperature (℃)", row=1, col=2)
+    fig.update_yaxes(title_text="", row=1, col=2)
 
-    # sizing
-    state_fig.update_layout(height=350)
-    dist_fig.update_layout(height=350)
-
-    return state_fig, dist_fig
+    return fig
 
 
 if __name__ == "__main__":
